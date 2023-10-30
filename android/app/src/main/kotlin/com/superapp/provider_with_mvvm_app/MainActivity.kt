a4b5d1215c1be15721f226aa738bde69b800b5a0
= <mustafatahirhussein@gmail.com>
2023-10-30 18:49:04 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/superapp/provider_with_mvvm_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.superapp.provider_with_mvvm_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:provider_with_mvvm_app/app.dart';
+
+main() {
+  runApp(App());
+}
+
+/// parse data from https://reqres.in/
